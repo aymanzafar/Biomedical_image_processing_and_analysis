64a4006e968f41828490f1c11f3c5dc2f9fd1c71
Ayman <99040487+aymanzafar@users.noreply.github.com>
2025-07-10 20:35:03 +0600

    Delete External files directory

--- External files/Untitled22.m
+++
@@ -1,1 +0,0 @@
-openExample('images/ContrastEnhancementExample')
--- External files/Untitled3.m
+++
@@ -1,4 +0,0 @@
-pout = imread("pout.tif");
-pout_imadjust = imadjust(pout);
-pout_histeq = histeq(pout);
-pout_adapthisteq = adapthisteq(pout);
